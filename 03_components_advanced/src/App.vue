027e6b426259ec11918aa5111827d41365841d7c
amasotti <toniomasotti@gmail.com>
2021-06-06 13:00:15 +0200

    added local component

--- 03_components_advanced/src/App.vue
+++ 03_components_advanced/src/App.vue
@@ -33,11 +33,11 @@
   </div>
 
 </template>
-import TheHeader from './components/TheHeader.vue';
-app.component('the-header', TheHeader);
 
 <script>
+import TheHeader from './components/TheHeader.vue';
 export default {
+  components: {"the-header" : TheHeader},
   data() {
     return {
       activeUser: {
