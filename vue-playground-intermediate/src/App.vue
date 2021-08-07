4b1d2e256f7203c080d64df6832cdcbd98698c18
amasotti <toniomasotti@gmail.com>
2021-08-07 10:03:20 +0200

    play around with $refs

--- vue-playground-intermediate/src/App.vue
+++ vue-playground-intermediate/src/App.vue
@@ -1,15 +1,43 @@
 <template>
+<div>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+<form>
+  <label for="john">Enter something dude: </label>
+ <input type="text" name="john" ref="mytext"/>
+</form>
+<button @click="updateText">Set text</button>
+<p>You entered: </p>
+<div id="wye" >
+{{returnedText}}
+</div>
+<notes />
+</div>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import notes from './components/refs.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,notes
+  },
+  data() {
+    return {
+      text : '',
+      returnedText : null
+    }
+  },
+  methods: {
+    updateText() {
+      console.dir(this.$refs.mytext)
+      this.returnedText = this.$refs.mytext.value;
+      this.$refs.mytext.size = 39;
+    }
   }
 }
 </script>
