e56ca56d1b550581de96484624bfe926333d58be
Antonio Masotti <masotti@bikeleasing.de>
2021-10-23 10:16:27 +0200

    init project learning resources

---
+++ 04_learningResources_project/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+  <div>
+    Welcome to vue
+  </div>
+
+<div v-for="resource in storedResources" :key="resource.id">
+    <resource-item :resource="resource"/>
+</div>
+  
+
+</div>
+</template>
+
+
+<script>
+
+import ResourceItem from './components/learningResources/ResourceItem.vue';
+
+export default {
+  components: {
+    ResourceItem
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'vue-official',
+          title: 'Vuejs documentation',
+          description: 'The official guide and API docs to vue js',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google-search',
+          title: 'Google Search Engine',
+          description: 'The best friend of a programmer',
+          link: 'https://google.de'
+        },
+
+      ]
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ 04_learningResources_project/src/components/learningResources/ResourceItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <h3 class="resourceTitle">{{resource.title}}</h3>
+    <hr>
+    <div class="resourceDescription">
+        <p>{{resource.description}}</p>
+        <a :href="resource.link">Visit now!</a>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'resource-item',
+  data() {
+
+    return {
+      
+    }
+  
+  },
+  props: [ 'resource']
+}
+</script>
+
+
+
+<style scoped>
+
+.container{
+    position: relative;
+    display: inline-flex;
+    flex-direction: column;
+    width: 50%;
+    margin: 1rem 0;
+    padding: 30px 30px;
+    border: 1px solid rgba(0, 0, 0, .75);
+    justify-content: center;
+    text-align: center;
+}
+</style>
